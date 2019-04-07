805a8edd1ad4e9f2e5ee4be5c58bd50e3dc3ef93
van nguyen <nguyenvan0525@gmail.com>
2019-04-07 22:31:01 +0700

    DTO Khach Hang

---
+++ QuanLiSach/DTO/DTO_KhachHang.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class DTO_KhachHang
+    {
+        public string MaKH { get; set; }
+        public string HotenKH { get; set; }
+        public string Diachi { get; set; }
+        public string DienThoai { get; set; }
+        public string Email { get; set; }
+        public DTO_KhachHang(string maKH, string hotenKH, string diachi, string dienThoai, string email)
+        {
+
+            this.MaKH = maKH;
+            this.HotenKH = hotenKH;
+            this.Diachi = diachi;
+            this.DienThoai = dienThoai;
+            this.Email = email;
+
+        }
+    }
+}
