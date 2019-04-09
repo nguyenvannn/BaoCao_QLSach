634b480cc670266257dadfe043755eb96a1d2c61
van nguyen <nguyenvan0525@gmail.com>
2019-04-09 08:16:00 +0700

    San Pham

---
+++ QuanLiSach/DTO/DTO_SanPham.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class DTO_SanPham
+    {
+        public int Id { get; set; }
+        public string MaSP { get; set; }
+        public string TenSP { get; set; }
+        public double Dongia { get; set; }
+        public DTO_SanPham(int id, string masp, string tensp, double DonGia)
+        {
+            this.Id = id;
+            this.MaSP = masp;
+            this.TenSP = tensp;
+            this.Dongia = DonGia;
+
+        }
+    }
+}
