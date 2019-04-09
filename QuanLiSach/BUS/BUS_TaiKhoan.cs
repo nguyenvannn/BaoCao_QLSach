a5a27ab4d82faebfbd6a47eb968bcc0a51ddf633
van nguyen <nguyenvan0525@gmail.com>
2019-04-09 07:59:57 +0700

    Tai Khoan

---
+++ QuanLiSach/BUS/BUS_TaiKhoan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO;
+using DTO;
+
+namespace BUS
+{
+    public class BUS_TaiKhoan
+    {
+        DAO_TaiKhoan emp = new DAO_TaiKhoan();
+        public List<DTO_TaiKhoan> GetData(string sql)
+        {
+            return emp.GetData(sql);
+        }
+    }
+}
