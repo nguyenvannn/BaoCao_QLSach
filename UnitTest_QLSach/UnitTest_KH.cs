3e81839364c8e4ec26b0540aeb2ef34cd9b9ff43
vodaolananh <1554050005anh@ou.edu.vn>
2019-04-14 14:39:58 +0700

    UnitTest_KH thử giá trị

--- UnitTest_QLSach/UnitTest_KH.cs
+++ UnitTest_QLSach/UnitTest_KH.cs
@@ -3,10 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DAO;
+using DTO;
 
 namespace UnitTest_QLSach
 {
-    class UnitTest_KH
+    [TestClass]
+    public class UnitTest_KH
     {
+        DAO_Khachhang dao;
+        DTO_KhachHang KhachHang1;
+        DTO_KhachHang KhachHang2;
+        DTO_KhachHang KhachHang3;
+        DTO_KhachHang KhachHang4;
+        DTO_KhachHang KhachHang5;
+        [TestInitialize]
+        public void SetUp()
+        {
+            dao = new DAO_Khachhang();
+            KhachHang1 = new DTO_KhachHang
+            {
+                MaKH = "KH_001",
+                HotenKH = "Khách hàng test 1",
+                Diachi = "BT",
+                DienThoai = "1",
+                Email = "1"
+
+            };
+            KhachHang2 = new DTO_KhachHang
+            {
+                MaKH = "KH_002",
+                HotenKH = "Khách hàng test 2",
+                Diachi = "BT",
+                DienThoai = "1",
+                Email = "1"
+            };
+            KhachHang3 = new DTO_KhachHang
+            {
+                MaKH = "KH_003",
+                HotenKH = "Khách hàng test 3",
+                Diachi = "BT",
+                DienThoai = "1",
+                Email = "1"
+            };
+            KhachHang4 = new DTO_KhachHang
+            {
+                MaKH = "KH_004",
+                HotenKH = "Khách hàng test 4",
+                Diachi = "BT",
+                DienThoai = "1",
+                Email = "1"
+            };
+            KhachHang5 = new DTO_KhachHang
+            {
+                MaKH = "KH_005",
+                HotenKH = "Khách hàng test 5",
+                Diachi = "BT",
+                DienThoai = "1",
+                Email = "1"
+            };
+
+        }
+
     }
 }
